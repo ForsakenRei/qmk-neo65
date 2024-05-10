d1937f01c31e09fc2a3393bf560ed85f85bc5023
ForsakenRei <23041178+ForsakenRei@users.noreply.github.com>
2024-05-10 14:34:49 -0400

    fix: backslash mapping

--- keyboards/neo/neo65/ansi/keymaps/shigure/keymap.c
+++ keyboards/neo/neo65/ansi/keymaps/shigure/keymap.c
@@ -58,28 +58,28 @@ bool _isWinKeyDisabled = false;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = LAYOUT_hot(
-		QK_GESC, KC_1,    KC_2,    KC_3,   KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_DEL,   
+		QK_GESC, KC_1,    KC_2,    KC_3,   KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC, KC_DEL,   
 		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, HOME_PU,
 		CAP_L1,  KC_A,    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           END_PD,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   RSFT_L3,
 		KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             FN_L2,   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RIGHT
 	),
 	[1] = LAYOUT_hot(
-		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,  KC_INS,
+		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,  KC_INS,
 		_______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR,
 		_______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
 		_______, _______, _______, KC_CALC, _______, _______, SERACH,  TSK_MGR, _______, _______, _______, _______,          _______, KC_MUTE,
 		_______, _______, _______,                   KC_DEL,                             _______, CTL_APP,          _______, _______, _______
 	),
 	[2] = LAYOUT_hot(
-		_______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PMNS, KC_PPLS, SFT_DEL, SFT_DEL, KC_SLEP,
+		_______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PMNS, KC_PPLS, _______, SFT_DEL, KC_SLEP,
 		_______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_NO,   KC_PAST, KC_PSLS, _______, _______,
 		_______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_NO,   KC_NO,   KC_PENT,          _______,
 		_______, _______, _______, KC_CALC, _______, _______, _______, KC_P0,   KC_P0,   KC_PDOT, _______, _______,          _______, _______,
 		_______, GU_TOGG, _______,                   KC_DEL,                             _______, _______,          _______, _______, _______
 	),
 	[3] = LAYOUT_hot(
-		RGB_TOG, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MUTE, _______, _______, KC_F13,  KC_F14,  _______, _______, RGB_SAI,
+		RGB_TOG, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MUTE, _______, _______, KC_F13,  KC_F14,  QK_BOOT, _______, RGB_SAI,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT, RGB_SAD,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, EE_CLR,           RGB_HUI,
 		_______, _______, _______, _______, _______, _______, _______, DC_MUTE, _______, _______, _______, QK_RBT,           RGB_MOD, RGB_HUD,
