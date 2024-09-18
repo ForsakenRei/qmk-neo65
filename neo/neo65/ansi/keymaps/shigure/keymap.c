a7649c600291c4356b52164b416bfbc309e5ac4c
ForsakenRei <23041178+ForsakenRei@users.noreply.github.com>
2024-09-18 19:34:58 -0400

    chore: qmk 0.26.0 breaking change Aug 2024

--- neo/neo65/ansi/keymaps/shigure/keymap.c
+++ neo/neo65/ansi/keymaps/shigure/keymap.c
@@ -52,6 +52,10 @@ enum td_keycodes
 #define SFT_DEL S(KC_DEL)
 #define HOME_PU TD(HOME_PAGEUP)
 #define END_PD TD(END_PAGEDOWN)
+#define SCR_LFT G(S(KC_LEFT))
+#define SCR_RHT G(S(KC_RGHT))
+#define VD_LEFT G(C(KC_LEFT))
+#define VD_RGHT G(C(KC_RIGHT))
 
 bool _isWinKeyDisabled = false;
 
@@ -67,16 +71,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[1] = LAYOUT_hot(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,  KC_INS,
 		_______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR,
-		_______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-		_______, _______, _______, KC_CALC, _______, _______, SERACH,  TSK_MGR, _______, _______, _______, _______,          _______, KC_MUTE,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, VD_LEFT, SCR_LFT, SCR_RHT, VD_RGHT, _______, _______, _______,          _______,
+        _______, KC_WBAK, KC_BSPC, KC_WFWD, _______, _______, SERACH,  TSK_MGR, _______, _______, _______, _______,          _______, KC_MUTE,
 		_______, _______, _______,                   KC_DEL,                             _______, CTL_APP,          _______, _______, _______
 	),
 	[2] = LAYOUT_hot(
-		_______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PMNS, KC_PPLS, _______, SFT_DEL, KC_SLEP,
-		_______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_NO,   KC_PAST, KC_PSLS, _______, _______,
-		_______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_NO,   KC_NO,   KC_PENT,          _______,
-		_______, _______, _______, KC_CALC, _______, _______, _______, KC_P0,   KC_P0,   KC_PDOT, _______, _______,          _______, _______,
-		_______, GU_TOGG, _______,                   KC_DEL,                             _______, _______,          _______, _______, _______
+		_______, KC_1,    KC_2,    KC_3,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, SFT_DEL, KC_SLEP,
+		_______, KC_4,    KC_5,    KC_6,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, KC_7,    KC_8,    KC_9,    _______, _______, VD_LEFT, SCR_LFT, SCR_RHT, VD_RGHT, _______, _______, _______,          _______,
+		_______, KC_0,    _______, KC_CALC, _______, _______, _______, TSK_MGR, _______, _______, _______, _______,          KC_BSPC, _______,
+		_______, GU_TOGG, _______,                   KC_DEL,                             _______, _______,          KC_WBAK, KC_F5,   KC_WFWD
 	),
 	[3] = LAYOUT_hot(
 		RGB_TOG, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MUTE, _______, _______, KC_F13,  KC_F14,  QK_BOOT, _______, RGB_SAI,
@@ -325,7 +329,7 @@ const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC
 const key_override_t alt_f12_override = ko_make_basic(MOD_BIT(KC_RCTL), KC_EQL, A(KC_F12));
 const key_override_t shift_f12_override = ko_make_basic(MOD_BIT(KC_RCTL), KC_MINS, S(KC_F12));
 
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t *key_overrides[] = {
     &delete_key_override,
     &alt_f12_override,
     &shift_f12_override,
